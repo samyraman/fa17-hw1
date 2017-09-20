af9a8c5e0e60208185623c56fda29dd3d46982ce
Samy <sraman@berkeley.edu>
2017-09-19 20:22:27 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,12 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    arr = Array.new(a)
+    arr.map! { |x| Integer(x) + 2 }
+    arr.uniq!
+    arr.keep_if { |x| x.even? and x <= 10}
+    arr.inject(0) {|sum, x|  sum + x }
+    
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,16 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  (a+b)*(a+b)
 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  a.sort!
+  a.map! {|x| x + 1 }
 
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  first_name + " " + last_name
 
 end
 
